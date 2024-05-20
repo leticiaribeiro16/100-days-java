272be09c0ea0427bd77e06588bf39b7bb3b256b8
Letícia Ribeiro <103691985+leticiaribeiro16@users.noreply.github.com>
2024-05-20 20:23:27 -0300

    Create day1.java
    First challenge

---
+++ day1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        scanner.close();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
